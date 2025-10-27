7cff5ad5cacf1e644805f2443d34bb794f9fa140
Gabigo401 <gabrielchoque017@gmail.com>
2025-10-26 21:07:08 -0300

    Primeiro commit

---
+++ src/projetojava/main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetojava;
+
+import View.Cadastrar;
+
+/**
+ *
+ * @author unifgchoque
+ */
+public class main {
+    public static void main(String[] args) {
+        Cadastrar c = new Cadastrar();
+        
+        c.setVisible(true);
+    }
+}
